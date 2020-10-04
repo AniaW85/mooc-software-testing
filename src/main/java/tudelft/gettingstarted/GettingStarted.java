92ff4861681af5bcb7c31c290a4bddae13a945f1
Anna Wawrusiewicz <A.Wawrusiewicz@fugro.com>
2020-10-04 21:47:12 +0200

    Complete the test class for addFive

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -1,7 +1,17 @@
 package tudelft.gettingstarted;
 
 public class GettingStarted {
-    public int addFive (int number) {
+    public int age;
+
+    public GettingStarted() {
+        this.age = 0;
+    }
+
+    public int addFive(int number) {
         return number + 5;
+    }
+
+    public GettingStarted(int ageToSet) {
+        this.age = ageToSet;
     }
 }
--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -7,21 +7,30 @@ public class GettingStartedTest {
 
     @Test
     public void addFiveTo20() {
-        int result = new GettingStarted().addFive(20);
+        int toAdd = 20;
+        GettingStarted objectGettingStarted = new GettingStarted();
+            var result = objectGettingStarted.addFive(toAdd);
         Assertions.assertEquals(25,result);
+        Assertions.assertEquals(0,objectGettingStarted.age);
     }
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
+
+    @Test
+    public void addFiveTo100() {
+        int result = new GettingStarted(25).addFive(100);
+        Assertions.assertEquals(105, result);
+    }
 }
