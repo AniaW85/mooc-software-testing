ee1e40d08a5c046c5b5880c68e89c80063c81c6c
Anna Wawrusiewicz <A.Wawrusiewicz@fugro.com>
2020-10-15 23:11:45 +0200

    Quiz: Topics 1 & 2 Practical exercise #1

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,24 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest
+    @CsvSource({"xxgggxx, true", "xxgg, true", "gg, true", "ggg, true", "xxggxx, true", "abcd,true"})
+    public void HappyFlowTest (String input, Boolean expectedResult){
+        GHappy happy = new GHappy();
+        Boolean result = happy.gHappy(input);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"xgx, false", "g, false", "gxg, false", "xxgxx, false", "xxggyygxx, false"})
+    public void UnhappyFlowTest (String input, Boolean expectedResult) {
+        GHappy happy = new GHappy();
+        Boolean result = happy.gHappy(input);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
